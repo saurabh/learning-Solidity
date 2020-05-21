5683b67e3b0c6fe5a4cb42b64386b1dc91e9091f
Saurabh W <w.saurabh@protonmail.com>
2020-05-21 17:18:50 +0400

    Lottery comtract complete

--- Course-2/lottery-project/Lottery.sol
+++ Course-2/lottery-project/Lottery.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.6.6;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] public players;
+    
+    modifier restricted() {
+        require(msg.sender == manager, 'You are not the contract manager.');
+        _;
+    }
     
     constructor() public {
         manager = msg.sender;
@@ -13,8 +18,18 @@ contract Lottery {
         players.push(msg.sender);
     }
     
-    function random() private view returns(uint) {
+    function psuedoRandom() private view returns(uint) {
         return uint(keccak256(abi.encodePacked(block.difficulty, players, now)));
+    }
+    
+    function pickWinner() public restricted {
+        uint index = psuedoRandom() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+    
+    function getPlayers() public view returns(address payable[] memory) {
+        return players;
     }
     
     receive() payable external {
