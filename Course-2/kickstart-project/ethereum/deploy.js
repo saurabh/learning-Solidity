046b154ddd205dc5df27ef80ebe1a34853e6b10f
Saurabh W <w.saurabh@protonmail.com>
2020-06-16 01:10:24 +0400

    redeployed contract

--- Course-2/kickstart-project/ethereum/deploy.js
+++ Course-2/kickstart-project/ethereum/deploy.js
@@ -19,7 +19,7 @@ const deploy = async () => {
     .deploy({ data: '0x' + campaignFactoryObject.evm.bytecode.object }) // add 0x bytecode
     .send({ from: accounts[0] }); // remove 'gas'
 
-  console.log('Contract deployed to: ', txn.options.address); // Deployed to 0xDBB3d41F91aEBdDAF0d01e8D041d776248cc2849 on Rinkeby
+  console.log('Contract deployed to: ', txn.options.address); // Deployed to 0x19046f91357Ff5fa028634f092B1f8F1E82d4350 on Rinkeby
 };
 
 deploy();
--- Course-2/kickstart-project/frontend/factory.js
+++ Course-2/kickstart-project/frontend/factory.js
@@ -1,6 +1,6 @@
 import web3 from './utils/getWeb3';
 import CampaignFactoryObject from './build/CampaignFactory.json';
-const address = '0xDBB3d41F91aEBdDAF0d01e8D041d776248cc2849';
+const address = '0x19046f91357Ff5fa028634f092B1f8F1E82d4350';
 
 const factoryConstructor = () => {
   return new web3.eth.Contract(CampaignFactoryObject.abi, address);
